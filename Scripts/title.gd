2a29b05f14f33d4f28e4639cebbf4aaf144dd094
oyasso <yassondevocht@protonmail.com>
2026-04-22 00:00:29 +0200

    [Feat] intro text update

--- Scripts/title.gd
+++ Scripts/title.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var background = $Background
 @onready var fade = $Fade
 @onready var intro_text = $IntroText
+@onready var end_text = $EndText
 @onready var next_button = $NextButton
 
 var text_speed = 10.0
@@ -11,6 +12,15 @@ func show_text():
 	intro_text.show()
 	var tween : Tween = create_tween()
 	tween.tween_property(intro_text, "visible_ratio", 1.0, intro_text.text.length()/text_speed).from(0.0)
+	await tween.finished
+	await get_tree().create_timer(1.5).timeout
+	intro_text.hide()
+	show_end_text()
+
+func show_end_text():
+	end_text.show()
+	var tween : Tween = create_tween()
+	tween.tween_property(end_text, "visible_ratio", 1.0, end_text.text.length()/text_speed).from(0.0)
 	await tween.finished
 	next_button.show()
 
